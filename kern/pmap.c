fd2d10b8deebdef38b83eb3f55d82cf773e1329c
Gong Linyuan <gonglinyuan@hotmail.com>
2019-01-20 15:40:34 +0800

    fix

--- kern/pmap.c
+++ kern/pmap.c
@@ -229,6 +229,7 @@ mem_init(void)
 	// we just set up the mapping anyway.
 	// Permissions: kernel RW, user NONE
 	// Your code goes here:
+	boot_map_region(kern_pgdir, KERNBASE, -KERNBASE, 0, PTE_W);
 
 	// Initialize the SMP-related parts of the memory map
 	mem_init_mp();
