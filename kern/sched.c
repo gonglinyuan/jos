53b14f88405e4d2bb1798a41e0619928cdade28e
Gong Linyuan <gonglinyuan@hotmail.com>
2019-01-21 00:40:17 +0800

    debug

--- kern/sched.c
+++ kern/sched.c
@@ -32,15 +32,18 @@ sched_yield(void)
 	envid_t curenv_id = curenv ? curenv->env_id : 0;
 	for (envid_t i = curenv_id + 1; i < NENV; ++i) {
 		if (envs[i].env_status == ENV_RUNNABLE) {
+			cprintf("yield %u\n", curenv - envs);
 			env_run(envs + i);
 		}
 	}
 	for (envid_t i = 0; i <= curenv_id; ++i) {
 		if (envs[i].env_status == ENV_RUNNABLE) {
+			cprintf("yield %u\n", curenv - envs);
 			env_run(envs + i);
 		}
 	}
 	if (curenv && curenv->env_status == ENV_RUNNING) {
+		cprintf("yield %u\n", curenv - envs);
 		env_run(curenv);
 	}
 	
